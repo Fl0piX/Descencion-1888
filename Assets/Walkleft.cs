e007ada969fe91208a7456fede2b52cf78fa8f98
Fl0piX <richar_5@epita.fr>
2016-12-05 22:18:37 +0100

    Upload du projet

---
+++ Assets/Walkleft.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Walkleft : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			Animator anim = GetComponent<Animator> ();
+			if (null != anim) {
+				anim.Play ("Left");
+			}
+		}
+		if (Input.GetKeyUp (KeyCode.LeftArrow)) {
+			Animator anim = GetComponent<Animator> ();
+			if (null != anim) {
+				anim.Play ("idleleft");
+			}
+		}
+	}
+}
---
+++ Assets/movey.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movey : MonoBehaviour {
+
+	Vector2 dest = Vector2.zero;
+	public float speed = 0.03f;
+	// Use this for initialization
+	void Start () {
+		dest = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
+		GetComponent<Rigidbody2D>().MovePosition(p);
+
+		if ((Vector2)transform.position == dest) {
+			if (Input.GetKey(KeyCode.UpArrow))
+				dest = (Vector2)transform.position + Vector2.up;
+			if (Input.GetKey(KeyCode.DownArrow))
+				dest = (Vector2)transform.position - Vector2.up;
+		}
+	}
+}
