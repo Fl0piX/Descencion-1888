b1fa58c09ddd69de9bc08829d7be5aa9e04f52a9
Fl0piX <richar_5@epita.fr>
2017-01-26 13:52:31 +0100

    Ajout du prototype 3D

---
+++ test3d/Assets/CharacterStats.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CharacterStats : MonoBehaviour {
+
+    public float health = 100;
+    bool dealDamage;
+    bool substractOnce;
+    bool dead;
+
+    public float damageTimer = .4f;
+    WaitForSeconds damageT;
+
+    Animator anim;
+
+    public GameObject sliderPrefab;
+
+    Slider healthSlider;
+    RectTransform healthTrans;
+
+	// Use this for initialization
+	void Start () {
+        damageT = new WaitForSeconds(damageTimer);
+        anim = GetComponent<Animator>();
+
+        GameObject slid = Instantiate(sliderPrefab, transform.position, Quaternion.identity) as GameObject;
+        slid.transform.SetParent(GameObject.FindGameObjectsWithTag("Canvas").transform);
+        healthSlider = slid.GetComponentInChildren<Slider>();
+        healthTrans = slid.GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        healthSlider.value = health / 100;
+
+        Vector2 screenPoint = RectTransformUtility.WorldToScreenPoint(Camera.main, transform.position);
+        healthTrans.transform.position = screenPoint;
+
+        if(dealDamage)
+        {
+            if(!substractOnce)
+            {
+                health -= 30;
+                anim.SetTrigger("Hit");
+                substractOnce = true;
+            }
+
+            StartCoroutine("CloseDamage");
+        }
+	}
+}
