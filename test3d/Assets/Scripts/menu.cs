66633bb1255c97759da3ef545ef98824f61dbda7
Fl0piX <richar_5@epita.fr>
2017-02-28 15:36:07 +0100

    menu

--- test3d/Assets/Scripts/menu.cs
+++ test3d/Assets/Scripts/menu.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class menu : MonoBehaviour {
-
 	public void loadLevel()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+    public void son()
+    {
+        AudioSource music = GetComponent<AudioSource>();
+        if (!music.isPlaying)
+        {
+            music.Play();
+        }
+        else
+        {
+            music.Stop();
+        }
     }
 }
